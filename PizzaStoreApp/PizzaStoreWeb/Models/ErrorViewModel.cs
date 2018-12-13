1ee8685f51acc5235d74244b1a022411d23331a9
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-13 09:13:09 -0600

    Deteled console app. Created web interface.

---
+++ PizzaStoreApp/PizzaStoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PizzaStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
