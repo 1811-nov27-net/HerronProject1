b3d0e61d7f8990bd465989faffb3dfd41e83bcbd
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-14 09:21:19 -0600

    Implementing Repositories

---
+++ MVPizza/MVPizza/Repositories/IMVPizzaRepo.cs
@@ -0,0 +1,23 @@
+﻿using MVPizza.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVPizza.Repositories
+{
+    interface IMVPizzaRepo
+    {
+        Order GetOrderByID(int OrderID);
+        Store GetStoreByName(string StoreName);
+        User GetUserByUsername(string Username);
+        Address GetAddressById(int AddressID);
+        IList<Order> GetOrdersByUsername(string Username);
+        IList<Order> GetOrdersByStore(string StoreName);
+        IList<Order> GetOrdersByAddress(int AddressID);
+        Store GetStoreByZip(int Zip);
+        IList<Address> GetAddressesByUsername(string Username);
+        User GetUserByName(string FirstName, string LastName = null);
+        void PlaceOrder(Order order);
+        void AddAddressToUser(Address address, User user);
+    }
+}
---
+++ MVPizza/MVPizza/Repositories/MVPizzaRepo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVPizza.Models;
+
+namespace MVPizza.Repositories
+{
+    public class MVPizzaRepo : IMVPizzaRepo
+    {
+        public void AddAddressToUser(Address address, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Address GetAddressById(int AddressID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Address> GetAddressesByUsername(string Username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrderByID(int OrderID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Order> GetOrdersByAddress(int AddressID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Order> GetOrdersByStore(string StoreName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Order> GetOrdersByUsername(string Username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Store GetStoreByName(string StoreName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Store GetStoreByZip(int Zip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserByName(string FirstName, string LastName = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserByUsername(string Username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
