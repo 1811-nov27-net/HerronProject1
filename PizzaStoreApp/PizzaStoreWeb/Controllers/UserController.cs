8dbdc1faa2c47ccf4568287c7ca88ae9e358d2e2
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-17 11:15:28 -0600

    Building User Controller and Views.

--- PizzaStoreApp/PizzaStoreWeb/Controllers/UserController.cs
+++ PizzaStoreApp/PizzaStoreWeb/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PizzaStoreWeb.Controllers
@@ -11,6 +12,75 @@ namespace PizzaStoreWeb.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        // GET: Address/Create
+        public ActionResult AddAddress()
+        {
+            return View();
+        }
+
+        // POST: Address/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddAddress(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Address/Edit/5
+        public ActionResult EditAddress(int id)
+        {
+            return View();
+        }
+
+        // POST: Address/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditAddress(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Address/Delete/5
+        public ActionResult DeleteAddress(int id)
+        {
+            return View();
+        }
+
+        // POST: Address/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteAddress(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
